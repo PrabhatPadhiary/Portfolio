8049d37b69d77d2da8f570092ce0d38a05d17bde
Prabhat Padhiary <prabhatpadhiary@gmail.com>
2022-07-24 20:05:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//============================Typing Animation============================
+var typed = new Typed('.typing',{
+    strings:["Web Designer","UI/UX Developer","Web Developer","Content Writer"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+})
